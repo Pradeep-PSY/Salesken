96181b271c69d608e049b3875f0dc12060f4290f
Danny12244 <pradeep.py725@gmail.com>
2023-03-06 23:17:09 +0530

    practice set

---
+++ test.js
@@ -0,0 +1,15 @@
+
+function create(){
+    let a = 'cool'
+    // console.log(' I am '+ a)
+    function another(){
+        
+        console.log(' I am '+ a + 90 *90)
+    }
+    a = 'super cool'
+   return  another
+
+}
+let k = create()
+k()
+
