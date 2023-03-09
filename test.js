8e1d66090c9eea4c676eec5643ffe0fdf7e1fb01
Danny12244 <pradeep.py725@gmail.com>
2023-03-10 00:01:23 +0530

    update

--- test.js
+++ test.js
@@ -1,17 +1,41 @@
 
-function create(){
-    let a = 'cool'
-    // console.log(' I am '+ a)
-    function another(){
+// function create(){
+//     let a = 'cool'
+//     // console.log(' I am '+ a)
+//     function another(){
         
-        console.log(' I am '+ a + 90 *90)
-    }
-    a = 'super cool'
-   return  another
+//         console.log(' I am '+ a + 90 *90)
+//     }
+//     a = 'super cool'
+//    return  another
 
-}
-let k = create()
-k()
+// }
+// let k = create()
+// k()
 
 //holi today
 //again holiday
+
+
+function add(a){
+    return function(b){
+        return a + b
+    }
+}
+
+let x = add(5)
+
+console.log(x(4))
+
+function user(name){
+    return function (age){
+        console.log(`My name is ${name} and my Age is ${age}`)
+    }
+}
+
+let op = user('Rahul')
+op(24)
+
+function addd(a,b){
+    return a+b
+}
