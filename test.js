e7a8024aaa77789538bace67812ce41a43db4ff6
Danny12244 <pradeep.py725@gmail.com>
2023-03-11 23:55:20 +0530

    update

--- test.js
+++ test.js
@@ -39,3 +39,35 @@ op(24)
 function addd(a,b){
     return a+b
 }
+
+
+function prod(x) {
+    x = x + 5
+    return z =>(
+        x * z
+    )
+}
+
+console.log(prod(5)(10))
+
+
+class Registration {
+    constructor(name, age){
+        this.name = name;
+        this.age = age
+    }
+    get naam (){
+        return [this.name, this.age];
+    }
+    set nam (nam){
+        this.name = nam
+    }
+}
+
+let person = new Registration('Rahul', 22)
+
+console.log(person.naam)
+
+person.nam = 'Arun'
+
+console.log(person.naam)
