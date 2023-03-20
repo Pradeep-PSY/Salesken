c84395b05f1da4f1dab52272dc53a389679869b9
Danny12244 <pradeep.py725@gmail.com>
2023-03-20 23:57:58 +0530

    work

--- op.js
+++ op.js
@@ -9,9 +9,13 @@ let head = new Node(1);
 let node2 = new Node(2)
 let node3 = new Node(3)
 let node4 = new Node(4)
+let node5 = new Node(5)
+let node6 = new Node(6)
 
 head.next = node2;
 node2.next = node3;
 node3.next = node4;
+node4.next = node5;
+node5.next = node6;
 
 console.log(head)
